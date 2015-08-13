abe37ab92a47939b5b5051cff1f64fb0cdca0bed
Marco <bartel@electronic-shop.lu>
2015-08-13 14:49:33 +0200

    build script extended with clean up beforehand

--- build_debian.sh
+++ build_debian.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
+# cleaning
+rm deb_dist -Rfv
+rm dist -Rfv
+# building
 python setup.py --command-packages=stdeb.command sdist_dsc --with-python2=True --with-python3=True --no-python2-scripts=True bdist_deb
